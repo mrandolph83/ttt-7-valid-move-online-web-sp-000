5cefcc5073061d3a091ee1f5e357c2193c462954
Matt Smith <mrandolph83@gmail.com>
2020-11-11 20:11:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,13 +2,12 @@
 # Accept a board and an index
 def valid_move?(board, index)
 
+index.between?(0,8) && !position_taken?(board, index)
 
-   if board[index] == " " || board[index] == ""
-     true
-
-  else board[index] == "X" || board[index] == "O"
-    false
 end
+
+def position_taken?(board, index)
+  board[index] != " "
 end
 
 
